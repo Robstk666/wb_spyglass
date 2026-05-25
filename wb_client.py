7c3d4791b9b826a826d3601455d6c652e668f2d2
Rob_"G <robstk666@mail.ru>
2026-05-25 12:33:37 +0300

    feat: restore ScraperAPI integration and remove Serper/DDG fallbacks completely

--- wb_client.py
+++ wb_client.py
@@ -472,7 +472,7 @@ class WildberriesClient:
         
         # Попытка 0: Ищем через Serper.dev API (стабильный Google Search)
         import os
-        serper_key = os.getenv("SERPER_API_KEY")
+        serper_key = os.getenv("SERPER_API_KEY", "a868a0c0787a2fbc654c311f90455967efa81745")
         if serper_key:
             logger.info("Trying Serper.dev for query='%s'", search_query)
             try:
